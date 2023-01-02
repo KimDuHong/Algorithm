4acdb7dbe3c794ccea0f0aa9f77f0f8109fd1a9b
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-02 16:28:30 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int i = 0;
+bool cmp(string &a, string &b){
+    if (a[i] > b[i]) return false;
+    else if (a[i] < b[i]) return true;
+    else if (a[i] == b[i]){
+        if(a > b) return false;
+        else return true;
+    }
+}
+vector<string> solution(vector<string> strings, int n) {
+    i = n;
+    sort(strings.begin(),strings.end(),cmp);
+    return strings; 
+}
