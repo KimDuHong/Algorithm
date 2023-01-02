b5381d990dd6b2d1fd2307a35a956b5eb36cc614
KimDuHong <kdh97048@gmail.com>
2023-01-02 15:27:44 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> day;
+    for ( int i = 0; i < speeds.size(); i ++)
+    {
+        int temp = 100 - progresses[i];
+        if ( temp % speeds[i] != 0 )
+        {
+            temp = temp / speeds[i] + 1;
+        }else temp = temp / speeds[i];
+        day.push_back(temp);
+    }
+    queue<int> q;
+    for (int d:day)
+    {
+        if (q.empty())
+        {
+        q.push(d);
+        }
+        else
+        {
+            if (q.front() < d)
+            {
+                answer.push_back(q.size());
+                while( !q.empty())
+                {
+                    q.pop();
+                }
+                q.push(d);
+            }
+            else 
+            {
+                q.push(d);
+            }
+        }
+    }
+    if (!q.empty()) answer.push_back(q.size());
+    return answer;
+}
