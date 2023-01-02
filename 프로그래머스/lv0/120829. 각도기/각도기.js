17556951cceecf247d4ccd5172c770916ed77a56
KimDuHong <kdh97048@gmail.com>
2023-01-02 15:16:14 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,18 @@
+function solution(angle) {
+    var answer = 0;
+    if (angle == 180) return 4;
+        if (angle <90)
+        {
+            answer = 1;
+        }
+    else if (angle == 90)
+        {
+            answer = 2;
+        }
+    else if (angle > 90)
+        {
+            answer = 3;
+        }
+  
+    return answer;
+}
