9a48d25e7f150239da4c0834402ff9f6e67c96c6
KimDuHong <kdh97048@gmail.com>
2023-01-06 17:36:13 +0900

    [level 2] Title: 올바른 괄호, Time: 6.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,10 @@
 def solution(s):
-    answer = True
     tmp = 0
     for i in s:
         if(i == "("):
             tmp +=1
         else:
             tmp-=1
-        
         if(tmp <0):
-            answer = False
             break
-    if not tmp:
-        answer = True
-    else:
-        answer = False
-    return answer
+    return False if tmp else True
