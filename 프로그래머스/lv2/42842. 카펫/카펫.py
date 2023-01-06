267f23143e007ae49c1f784a999a0cbc07f37d43
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-06 10:41:16 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    answer = []
+    area = brown+yellow
+    curX,curY = area,1
+    
+    while(1):
+        if not area / curX:
+            continue
+        curX = area / curY
+        count_brown = curX *2 + (curY-2)*2
+        if(count_brown == brown):
+            break
+        curY+=1
+    answer.append(curX)
+    answer.append(curY)
+    return answer
