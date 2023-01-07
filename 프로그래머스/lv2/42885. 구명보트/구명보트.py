4b3a7dfa0c0e7cf7b90e58f20759ba56775633c0
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-07 17:42:16 +0900

    [level 2] Title: 구명보트, Time: 7.64 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,12 +1,9 @@
 def solution(people, limit):
-    answer = 0
-    s = 0
-    r= len(people)-1
-    people = sorted(people)
+    answer,s,r = 0,0,len(people)-1
+    people.sort()
     while(s<=r):
         if people[r] + people[s] <= limit:
             s+=1
         r-=1
         answer+=1
-        
     return answer
