e8c934eb3f33c51bf195d640b7502783a238c23f
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-11 02:04:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -13,11 +13,10 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
         for(;temp.size() != n;){
             if ( arr1[i] %2 ==0){
                 temp += ' ';
-                arr1[i] /=2;
             }else {
                 temp += '#';
-                arr1[i] /=2;
             }
+            arr1[i] /=2;
         }
         reverse(temp.begin(), temp.end());
         answer.push_back(temp);
