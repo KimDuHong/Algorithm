1f2a4aba10c79d998f029c37ea82f0178c9bf6cf
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-11 10:49:39 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,43 @@
+def step1(id):
+    id = id.lower()
+    return id
+
+def step2(id):
+    tmp = ""
+    for i in id:
+        if (i >='0' and i <= '9') or (i >= 'a' and i  <= 'z'):
+            tmp+=i
+        elif i=='-' or i=='_' or i  == '.':
+            tmp+=i
+    return tmp
+
+def step3(id):
+    while ".." in id:
+        id = id.replace("..", ".")
+    return id
+
+def step4(id):
+    id = id[1:] if id[0] == '.' and len(id) > 1 else id
+    id = id[:-1] if id[-1] == '.' else id
+    return id
+
+def step5(id):
+    return id if id else "a"
+
+def step6(id):
+    if len(id) >=16:
+        id = id[:15]
+        if id[-1] == '.':
+            return id[:-1]
+        else:
+            return id
+    else:
+        return id
+    
+def step7(id):
+    while(len(id) <3):
+        id+=id[-1]
+    return id
+
+def solution(new_id):
+    return step7(step6(step5(step4(step3(step2(step1(new_id)))))))
