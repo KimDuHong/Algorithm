7c6d00207e1404afe111506537205c00a4b51d76
KimDuHong <kdh97048@gmail.com>
2023-01-15 22:48:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,11 @@
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    id_report = {id: [] for id in id_list}
+    report = list(set(report))
+    for i in report:
+        id_report[i.split()[1]].append(i.split()[0])
+    for item in id_report.values():
+        if len(item) >= k:
+            for j in item:
+                answer[id_list.index(j)]+=1
+    return answer
