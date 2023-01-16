bd5817e3699b464b100add5e6ebcf9129f013791
KimDuHong <kdh97048@gmail.com>
2023-01-17 01:02:20 +0900

    [level 1] Title: 최소직사각형, Time: 3.69 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,22 +1,9 @@
 def solution(sizes):
     answer = 0
-    data = []
-    for size in sizes:		 #data를 1차원 배열로 만들기
-        for i in size:
-            data.append(i)
-
-    data.sort(reverse=True)  #정렬을 통해 max min값을 구한다.
-    max = data[0]
-    min = data[len(data) - 1]
-
-    for size in sizes:
-        if min < size[0] and min < size[1]: #min값을 sizes요소들과 비교후 교체
-            if size[0] < size[1]:
-                min = size[0]
-            else:
-                min = size[1]
-
-
-    answer = min * max
+    array_a = []
+    array_b = []
+    for i in sizes:
+        array_a.append(max(i))
+        array_b.append(min(i))
+    answer = max(array_a) * max(array_b)
     return answer
-
