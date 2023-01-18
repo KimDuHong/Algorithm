2a226feaeb79e717fab11ad19664456331d74eed
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-18 19:01:44 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,3 +1,11 @@
 def solution(common):
-    return common[-1] + common[-1] - common[-2] if common[-1] - common[-2] == common[-2] - common[-3] else common[-1] * common[-1]/common[-2]
-   
+    # if 배열 첫번째 더하기 두번째는 세번째면 : 마지막 더하기(두번째 - 첫번째)
+    # if 배열 세번째 나누기 두번째는 배열 첫번째면 : 배열 세번째 곱하기 첫번째
+    answer=0
+    if common[2]-common[1]==common[1]-common[0]:
+        answer = common[-1]+common[1]-common[0]
+        return answer 
+    else:
+
+        answer=common[-1]*common[1]/common[0]
+        return answer
