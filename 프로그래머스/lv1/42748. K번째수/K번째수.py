4034ffcef0a318b1042e716ede6df6a9cfb16d4f
KimDuHong <kdh97048@gmail.com>
2023-01-19 00:35:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,5 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for i in commands:
-        answer.append(sorted(array[i[0]-1:i[1]])[i[2]-1])
-    return answer
+    return [sorted(array[a-1:b])[c-1] for a,b,c in commands]
