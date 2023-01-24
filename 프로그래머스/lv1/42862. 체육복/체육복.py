759350ada9881705618808533add7ea38a548d44
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2023-01-24 14:40:09 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,13 +1,14 @@
 def solution(n, lost, reserve):
     reserve_set = sorted([i for i in reserve if i not in lost])
     lost_set = sorted([i for i in lost if i not in reserve])
-    
-    for i in reserve_set:
-        if i-1 in lost_set:
-            lost_set.remove(i-1)
-        elif i+1 in lost_set:
-            lost_set.remove(i+1)
-            
-    return n - len(lost_set)
+    answer = n-len(lost_set)
+    for i in lost_set:
+        if i-1 in reserve_set:
+            reserve_set.remove(i-1)
+            answer+=1
+        elif i+1 in reserve_set:
+            reserve_set.remove(i+1)
+            answer+=1           
+    return answer
                 
     
