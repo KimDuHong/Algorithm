8087d7b6904c64b034a5a7bf7e950451c01a8b8f
KimDuHong <kdh97048@gmail.com>
2023-01-30 14:53:42 +0900

    [level 4] Title: 호텔 방 배정, Time: 459.12 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/README.md
+++ 프로그래머스/lv4/64063. 호텔 방 배정/README.md
@@ -1,10 +1,10 @@
 # [level 4] 호텔 방 배정 - 64063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063) 
 
 ### 성능 요약
 
-메모리: 47 MB, 시간: 449.80 ms
+메모리: 46.9 MB, 시간: 459.12 ms
 
 ### 구분
 
--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,16 +1,17 @@
 from collections import defaultdict
 import sys
 sys.setrecursionlimit(10000000)
-m = defaultdict(int)
 
-def find_room(n):
-    if m[n] ==0:
-        return n
-    else:
-        m[n] = find_room(m[n])
-        return m[n]
 def solution(k, room_number):
     answer = []
+    m = defaultdict(int)
+    def find_room(n):
+        if m[n] ==0:
+            return n
+        else:
+            m[n] = find_room(m[n])
+        return m[n]
+    
     for i in room_number:
         n = find_room(i)
         answer.append(n)
