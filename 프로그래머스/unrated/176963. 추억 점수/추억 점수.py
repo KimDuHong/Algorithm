656dfe54492c5992fe380f910193a90efbd0fc09
KimDuHong <125422608+KimDuHong@users.noreply.github.com>
2023-05-09 13:27:16 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,7 +1,8 @@
 def solution(name, yearning, photo):
     answer = [0] * len(photo)
+    value_dict = dict(zip(name,yearning))
     for idx,value in enumerate(photo):
         for k in name:
             if k in value:
-                answer[idx]+=yearning[name.index(k)]
+                answer[idx]+=value_dict[k]
     return answer
