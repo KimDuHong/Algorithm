3527798e2c31711c6218e034371371640377b75e
KimDuHong <125422608+KimDuHong@users.noreply.github.com>
2023-05-27 21:49:05 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,12 +1,9 @@
-def solution(cards1,cards2,goal):
-    while goal:
-        word=goal[0]
-        if cards1 and cards1[0]==word:
-            cards1=cards1[1:]
-        elif cards2 and cards2[0]==word:
-            cards2=cards2[1:]
+def solution(cards1, cards2, goal):
+    for word in goal:
+        if cards1 and cards1[0] == word:
+            cards1 = cards1[1:]
+        elif cards2 and cards2[0] == word:
+            cards2 = cards2[1:]
         else:
             return "No"
-        goal=goal[1:]
     return "Yes"
-    
